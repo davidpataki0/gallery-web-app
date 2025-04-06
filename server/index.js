6c06d1ebfcf917ca45eb68ea7647efb37e4cc12e
davidpataki0 <patakidavid2001@gmail.com>
2025-04-06 14:20:08 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -27,10 +27,10 @@ app.use('/api/photos', photoRoutes);
 
 // Statikus fájlok kiszolgálása (production környezetben)
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../client/public')));
   
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../client/public', 'index.html'));
   });
 }
 
